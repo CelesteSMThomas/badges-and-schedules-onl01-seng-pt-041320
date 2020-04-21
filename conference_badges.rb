500bc46a6713d36ae183f9dfc928f1dc5efc6201
Celeste Thomas <celestesmthomas@gmail.com>
2020-04-21 15:11:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,9 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.each_with_index do |name, room|
-    
+  new_array = []
+  array.each_with_index do |name, index|
+new_array.push("Hello, #{name}! You'll be assigned to room #{index}")    
 end
 end
 
