dc44165859e66ca759e8f9369d71cfe3ad1e7b2e
Celeste Thomas <celestesmthomas@gmail.com>
2020-04-21 02:55:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@
 def batch_badge_creator(array)
   new_array = []
     array.each do |name|
-       new_array <<  "Hello, my name is #{name}."
+       new_array.push  ("Hello, my name is #{name}.")
     end
     new_array
 end
