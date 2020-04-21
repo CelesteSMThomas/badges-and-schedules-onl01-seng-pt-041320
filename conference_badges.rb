7ef32a9d1e2a9f2bf7c1338bba349dfe98652323
Celeste Thomas <celestesmthomas@gmail.com>
2020-04-21 15:14:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 def assign_rooms(array)
   new_array = []
   counter = 1
-  array.each do |name, index|
+  array.each do |name|
 new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
 counter += 1
 end
